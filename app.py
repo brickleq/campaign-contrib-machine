c324e83377fc44f122d527fb56a660e160fc13a5
brickleq <pblequire@gmail.com>
2019-07-27 12:18:55 -0500

    debugging mysql flask connection

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 # MySQL connection string
 from config import user, password, host, port, dbname
-connect_string = f('mysql+mysqlconnector://{user}:{password}@{host}:{port}/{dbname}')
+connect_string = (f'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{dbname}')
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -26,6 +26,16 @@ CORS(app) # Development only--allow access to local and remote IP addresses
 
 app.config['SQLALCHEMY_DATABASE_URI'] = connect_string
 db = SQLAlchemy(app)
+
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
+
+t = Test(id=1, name='test name')
+db.session.add(t)
 
 if __name__ == "__main__":
     app.run(debug=True)
