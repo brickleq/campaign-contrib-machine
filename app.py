abf82ec2c2aeb3ebccc0e0cc53d0b343363006fe
brickleq <pblequire@gmail.com>
2019-07-27 11:47:05 -0500

    flask won't run in OSX

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
-import json
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from flask import (
-    Flask,
-    render_template,
-    url_for,
-    jsonify,
-    request,
-    redirect)
-from flask_cors import CORS 
+# Dependencies
+
+# import json
+# import sqlalchemy
+# from sqlalchemy.ext.automap import automap_base
+# from sqlalchemy.orm import Session
+# from sqlalchemy import create_engine, func
+# from flask import (
+#     Flask,
+#     render_template,
+#     url_for,
+#     jsonify,
+#     request,
+#     redirect)
+from flask import Flask
+from flask_cors import CORS # Development only--allow access to local and remote IP addresses
 from flask_sqlalchemy import SQLAlchemy
 
+# MySQL connection string
 from config import user, password, host, port, dbname
-connect_string = (f'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{dbname}')
+connect_string = f('mysql+mysqlconnector://{user}:{password}@{host}:{port}/{dbname}')
 
 # Create an instance of Flask
 app = Flask(__name__)
 CORS(app) # Development only--allow access to local and remote IP addresses
 
-engine = create_engine(connect_string)
+app.config['SQLALCHEMY_DATABASE_URI'] = connect_string
+db = SQLAlchemy(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
