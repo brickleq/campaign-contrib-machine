dfc0d37f775807b0353d0c5dd61a645db03f0b54
brickleq <pblequire@gmail.com>
2019-08-01 14:52:55 -0500

    testing geojson_to_mysql.py in ubuntu

--- data_processing/geojson_to_mysql2.py
+++ data_processing/geojson_to_mysql2.py
@@ -21,19 +21,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = connect_string
 
 db = SQLAlchemy(app)
 
-class contributions2(db.Model):
+class contributions3(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key = True)
     CMTE_ID = db.Column(db.String(16))
     TRANSACTION_PGI = db.Column(db.String(16))
     ZIP_CODE = db.Column(db.String(9))
     TRANSACTION_DT = db.Column(db.String(16))
-    TRANSACTION_AMT = db.Column(db.Float())
-    SUB_ID = db.Column(db.String(19), unique=True, primary_key=True)
+    TRANSACTION_AMT = db.Column(db.Float)
+    SUB_ID = db.Column(db.String(19))
     zipcode_5 = db.Column(db.String(16), nullable=False)
     CAND_ID = db.Column(db.String(16), nullable=True)
     CAND_PARTY = db.Column(db.String(16), nullable=True)
 
     def __repr__(self):
-        return "<contributions2: {}>".format(self.__main__)
+        return "<contributions3: {}>".format(self.__main__)
 db.create_all()
 
 df = pd.read_csv('contributions.csv',delimiter=',',encoding='utf-8')
@@ -44,7 +45,7 @@ for i in range(len(df)):
     TRANSACTION_PGI = df.loc[i, 'TRANSACTION_PGI'],
     ZIP_CODE = df.loc[i, 'TRANSACTION_PGI'],
     TRANSACTION_DT = df.loc[i, 'TRANSACTION_DT'],
-    TRANSACTION_AMT = float(df.loc[i, 'TRANSACTION_AMT']),
+    TRANSACTION_AMT = df.loc[i, 'TRANSACTION_AMT'],
     SUB_ID = df.loc[i, 'SUB_ID'],
     zipcode_5 = df.loc[i, 'zipcode_5'],
     CAND_ID = df.loc[i, 'CAND_ID'],
@@ -52,9 +53,9 @@ for i in range(len(df)):
     try:
         db.session.add(post)
         db.session.commit()
-        print('Success posting record for SUB_ID ' + str(df.loc[i, 'SUB_ID']))
+        print('Success posting record for SUB_ID ' + str(i+1))
     except:
-        print('An error occurred posting record for SUB_ID ' + str(df.loc[i, 'SUB_ID']))
+        print('ERROR posting record for SUB_ID ' + str(i+1))
 
 if __name__ == "__main__":
     app.run(debug=True)
