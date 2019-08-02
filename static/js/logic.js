db10debe3adb6e348ed543ec42b6d65513c6a292
Alexander Lecocq <alexander.m.lecocq@gmail.com>
2019-08-02 16:54:52 -0500

    parties dropdown now works

--- static/js/logic.js
+++ static/js/logic.js
@@ -28,45 +28,45 @@ function chooseOpacity(indicator, maximum) {
 var map_variant = 'donations_sum'
 
 // Grabbing our GeoJSON data..
-d3.json(link, function(data) {
-  // Creating a geoJSON layer with the retrieved data
-  var maximum = data.maximums[map_variant]
-  L.geoJson(data, {
-    // Style each feature (in this case a neighborhood)
-    style: function(feature) {
-      if (maximum < feature.properties[map_variant]){
-      }
-      return {
-        color: 'red',
-        fillOpacity: chooseOpacity(feature.properties[map_variant], maximum),
-        weight: 1
-      };
-    },
-    // Called on each feature
-    onEachFeature: function(feature, layer) {
-      // Set mouse events to change map styling
-      layer.on({
-        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-        mouseover: function(event) {
-          layer = event.target;
-          layer.setStyle({
-            fillOpacity: 1
-          });
-        },
-        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-        mouseout: function(event) {
-          layer = event.target;
-          layer.setStyle({
-            fillOpacity: chooseOpacity(feature.properties[map_variant], maximum)
-          });
-        }
-      });
-      // Giving each feature a pop-up with information pertinent to it
-      layer.bindPopup("<strong>Zipcode</strong> " + feature.properties.zipcode + "<hr> <strong>" + map_variant + ":</strong> $" + feature.properties[map_variant]);
+// d3.json(link, function(data) {
+//   // Creating a geoJSON layer with the retrieved data
+//   var maximum = data.maximums[map_variant]
+//   L.geoJson(data, {
+//     // Style each feature (in this case a neighborhood)
+//     style: function(feature) {
+//       if (maximum < feature.properties[map_variant]){
+//       }
+//       return {
+//         color: 'red',
+//         fillOpacity: chooseOpacity(feature.properties[map_variant], maximum),
+//         weight: 1
+//       };
+//     },
+//     // Called on each feature
+//     onEachFeature: function(feature, layer) {
+//       // Set mouse events to change map styling
+//       layer.on({
+//         // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+//         mouseover: function(event) {
+//           layer = event.target;
+//           layer.setStyle({
+//             fillOpacity: 1
+//           });
+//         },
+//         // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+//         mouseout: function(event) {
+//           layer = event.target;
+//           layer.setStyle({
+//             fillOpacity: chooseOpacity(feature.properties[map_variant], maximum)
+//           });
+//         }
+//       });
+//       // Giving each feature a pop-up with information pertinent to it
+//       layer.bindPopup("<strong>Zipcode</strong> " + feature.properties.zipcode + "<hr> <strong>" + map_variant + ":</strong> $" + feature.properties[map_variant]);
 
-    }
-  }).addTo(map);
-});
+//     }
+//   }).addTo(map);
+// });
 
 
  // PARTY SELECTOR 
@@ -77,18 +77,20 @@ d3.json(link, function(data) {
   console.log(selector)
   // Use the list of sample names to populate the select options
   d3.json('/api/parties/', function(data) {
-    
-    console.log(data.parties); 
-    
-    partyArray = data.parties
+    console.log(data.parties);     
+    var partyArray = data.parties;
 
-    partyArray.forEach(function(party) {
-      selector
-        .append("option")
-        .text(party)
-        .property("value", party);
-      
-    });
+    d3.select("#selectParty")
+      .selectAll("option")
+      .data(partyArray)
+      .enter()
+      .append("option")
+      .text(function(d) {
+        return d;
+      })
+      .property("value", function(d) {
+        return d;
+      });
     
     // // Use the first sample from the list to build the initial plots
     // const partyName = party[0];
